a6f04ea171893f6665be0dd5a2785df3e7f48b4e
ivandgc <ivandgc@hotmail.com>
2017-08-10 17:40:36 -0400

    Done.

--- spec/robot.spec.js
+++ spec/robot.spec.js
@@ -1,7 +1,7 @@
 'use strict';
 
 describe("Robot", function() {
-  var robot = new Robot();
+  var robot =  new Robot()
 
   it("robot bearing", function() {
     var directions = [ 'east', 'west', 'north', 'south' ];
